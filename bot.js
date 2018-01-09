555e824ce0d9963c7e070e48d7a66d24ff9830da
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2018-01-10 00:02:54 +0530

    Add files via upload

--- bot.js
+++ bot.js
@@ -2,9 +2,9 @@ var Steam = require("steam"),
 	fs = require("fs"),
 	util = require("util"),
     csgo = require("csgo"),
-    readlineSync = require("readline-sync"),
+    csv = require("fast-csv"),
     crypto = require("crypto");
-    var logger = fs.createWriteStream('output.txt', {
+    var logger = fs.createWriteStream('output.csv', {
   	            flags: 'a'
   	            })
 
@@ -13,6 +13,8 @@ var Steam = require("steam"),
     	util.log("Logging into "+username+" . . . .");
     	var botInstance = new Steam.SteamClient(),
     	steamU = new Steam.SteamUser(botInstance),
+    	steamF = new Steam.SteamFriends(botInstance),
+    	s_level,
     	csgoGameCoordinator = new Steam.SteamGameCoordinator(botInstance, 730),
     	gameCoordinatorHandle = new csgo.CSGOClient(steamU, csgoGameCoordinator, false),
     	logOnDetails = {
@@ -25,7 +27,12 @@ var Steam = require("steam"),
         	var rank;
         	var wins;
         	var p_rank;
-        	var steamlink ="http://steamcommunity.com/profiles/"+botInstance.steamID;
+        	steam64 = botInstance.steamID;
+        	steamF.getSteamLevel([steam64], function(result){
+        		s_level = result[steam64];
+
+        	});
+        	var steamlink ="http://steamcommunity.com/profiles/"+steam64;
         	if (response.eresult != Steam.EResult.OK) {
             	result ='Error in logging in!';
         	}
@@ -41,9 +48,12 @@ var Steam = require("steam"),
             	    rank = gameCoordinatorHandle.Rank.getString(profile.account_profiles[0].ranking.rank_id);
                 	wins = profile.account_profiles[0].ranking.wins;
                 	p_rank = profile.account_profiles[0].player_level;
-	                logger.write(username+" "+password+" "+rank+" "+wins+" "+p_rank+" "+steamlink+"\r\n");
+                	csv.write([[rank,p_rank,username,password,wins," ",s_level,steamlink," "," "]],{headers : true}).pipe(logger);
+                	logger.write("\r\n");
+	               // logger.write(username+" "+password+" "+rank+" "+wins+" "+p_rank+" "+steamlink+"\r\n");
 	                util.log("Successfully fetched info for " + username);
 	                console.log("----------------------------------------------------");
+	                gameCoordinatorHandle.exit();
             	    botInstance.disconnect();
                     });                
             });
@@ -56,6 +66,7 @@ var Steam = require("steam"),
 	var args = process.argv.slice(2);
 	getinfo(args[0],args[1]);
 	
+
 
 
 
